05793125d8632cbbb2b89a4eeeb3ff1b1227e4d2
420Godd <naveensenanayake1999@gmail.com>
2022-11-24 00:46:24 +0530

    frontend 02
    adding images and edits in the main page

--- AgriLK/frontend/src/App.js
+++ AgriLK/frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">AgriLK</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.image} alt={product.name} width="679" height="829"/>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
---
+++ AgriLK/frontend/src/data.js
@@ -0,0 +1,68 @@
+const data = {
+    products: [
+        {
+            name: 'Carrot',
+            slug: 'Carrot',
+            category:'Vegetables',
+            image:'/images/carrot.png',
+            price: 130,
+            countInStock: 50,
+            brand: 'local',
+            rating: 4.5,
+            numReviews: 300,
+            description: 'Organic cultivated carrot for whole sale and retail prices.'
+
+        },
+        {
+            name: 'Pumpkin',
+            slug: 'Pumpkin',
+            category:'Vegetables',
+            image:'/images/Pumpkin.jpg',
+            price: 430,
+            countInStock: 20,
+            brand: 'local',
+            rating: 4.8,
+            numReviews: 1000,
+            description: 'Organic cultivated Pumpkin for whole sale and retail prices.'
+
+        },
+        {
+            name: 'Beans',
+            slug: 'Beans',
+            category:'Vegetables',
+            image:'/images/Beans.jpg',
+            price: 130,
+            countInStock: 800,
+            brand: 'local',
+            rating: 3.5,
+            numReviews: 50,
+            description: 'Organic cultivated Beans for whole sale and retail prices.'
+
+        },
+        {
+            name: 'Cabbage',
+            slug: 'Cabbage',
+            category:'Vegetables',
+            image:'/images/Cabbage.jpg',
+            price: 130,
+            countInStock: 800,
+            brand: 'local',
+            rating: 3.5,
+            numReviews: 50,
+            description: 'Organic cultivated Cabbage for whole sale and retail prices.'
+
+        },
+
+
+
+
+
+    ],
+
+
+
+
+
+
+};
+export default data;

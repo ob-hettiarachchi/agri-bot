ebf6dc33f5e5e6bed372e6a45dbd32eb0a84974e
Chamudi R Wijesekera <chamudirw1802@gmail.com>
2022-11-24 17:41:40 +0530

    Add bootstrap

--- AgriLK/frontend/src/App.js
+++ AgriLK/frontend/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <header className="App-header">
-          <Link to="/">AgriLK</Link>
+        <header>
+          <Navbar bg="dark" varient="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>AgriLK</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights are reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );

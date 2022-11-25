e13e48984d992ec4f6dd4dff4bbf363c80863930
Chamudi R Wijesekera <chamudirw1802@gmail.com>
2022-11-25 13:54:49 +0530

    Create Loading and Message Component

--- AgriLK/frontend/src/screens/ProductScreen.js
+++ AgriLK/frontend/src/screens/ProductScreen.js
@@ -9,6 +9,9 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -40,7 +43,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
 
       //setProducts(result.data);
@@ -48,9 +51,9 @@ function ProductScreen() {
     fetchData();
   }, [slug]);
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox varient="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>

58669b3cf41d16a0ecbb3620814c2785c568d8d2
420Godd <naveensenanayake1999@gmail.com>
2022-11-28 19:23:23 +0530

    side bar and search menu
    side bar and search menu

--- AgriLK/frontend/src/components/Rating.js
+++ AgriLK/frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 function Rating(props) {
-  const { rating, numReviews } = props;
+  const { rating, numReviews, caption } = props;
   return (
     <div className="rating">
       <span>
@@ -60,6 +60,13 @@ function Rating(props) {
       <span>
         <span> {numReviews} reviews</span>
       </span>
+
+      <span> {numReviews} reviews</span>
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{' ' + numReviews + ' reviews'}</span>
+      )}
     </div>
   );
 }

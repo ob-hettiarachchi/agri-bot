c219f1a4a723169ca63ea9e3a699c7d4dd91422c
420Godd <naveensenanayake1999@gmail.com>
2022-11-30 23:17:17 +0530

    dele all

--- AgriLK/frontend/src/components/LoadingBox.js
+++
@@ -1,8 +0,0 @@
-import Spinner from 'react-bootstrap/Spinner';
-export default function LoadingBox() {
-  return (
-    <Spinner animation="border" role="statux">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-  );
-}
--- AgriLK/frontend/src/components/MessageBox.js
+++
@@ -1,5 +0,0 @@
-import Alert from 'react-bootstrap/Alert';
-
-export default function MessageBox(props) {
-  return <Alert varient={props.varient || 'info'}>{props.children}</Alert>;
-}
